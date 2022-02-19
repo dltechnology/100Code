9144ca24f5b0ad5a0ddde062489e44ce86d07dab
dltechnology <david.lee.technology@gmail.com>
2022-02-18 17:47:47 -0800

    Create main.py

---
+++ Racing_Turtles/main.py
@@ -0,0 +1,90 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width=600, height=600)
+
+wenkai = "navy blue"
+shreya = "light blue"
+louis = "orange"
+david = "light green"
+abby = "pink"
+phoebe = "light sea green"
+
+colors = [wenkai, shreya, david, abby, phoebe, louis]
+
+y = [-110, -60, -10, 40, 90, 140]
+
+all_turtles = []
+
+is_race_on = True
+
+for i in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.shapesize(3)
+    new_turtle.color(colors[i])
+    new_turtle.goto(x=-225, y=y[i])
+    all_turtles.append(new_turtle)
+
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 220:
+            is_race_on = False
+        rand_dist = random.randint(0, 20)
+        turtle.forward(rand_dist)
+
+
+
+
+#
+# print(user_bet)
+#
+# david = Turtle(shape="turtle")
+# david.penup()
+# david.shapesize(3)
+# david.goto(x=-225, y=-160)
+# david.color("lime green")
+#
+#
+# phoebe = Turtle(shape="turtle")
+# phoebe.penup()
+# phoebe.shapesize(3)
+# phoebe.color("light sea green")
+# phoebe.goto(x=-225, y=110)
+#
+#
+# wenkai = Turtle(shape="turtle")
+# wenkai.penup()
+# wenkai.shapesize(3)
+# wenkai.color("navy blue")
+# wenkai.goto(x=-225, y=60)
+#
+#
+# shreya = Turtle(shape="turtle")
+# shreya.penup()
+# shreya.shapesize(3)
+# shreya.color("light blue", "lavender")
+# shreya.goto(x=-225, y=10)
+#
+#
+# abby = Turtle(shape="turtle")
+# abby.penup()
+# abby.shapesize(3)
+# abby.color("pink")
+# abby.goto(x=-225, y=-40)
+#
+#
+# louis = Turtle(shape="turtle")
+# louis.penup()
+# louis.shapesize(3)
+# louis.color("white", "black")
+# louis.goto(x=-225, y=-90)
+#
+#
+
+
+
+
+screen.exitonclick()
